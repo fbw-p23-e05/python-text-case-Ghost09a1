38e2adefa2f82fbc435c0275be7979ce28cb672f
Ghost09a1 <139347108+Ghost09a1@users.noreply.github.com>
2023-08-31 15:01:59 +0200

    yoink

---
+++ task.py
@@ -0,0 +1,8 @@
+original_string = "The Quick BroWnFoX jumps over the laZy Dog!"# Input 
+uppercase_string = original_string.upper()# uppercase
+lowercase_string = original_string.lower()# lowercase
+
+# results:
+print("Original String:", original_string)
+print("String in uppercase:", uppercase_string)
+print("String in lowercase:", lowercase_string)
